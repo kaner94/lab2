e16a8e87c3c18c92cc4c180b06e814c46ec618e5
kaner94 <kanery@tcd.ie>
2016-11-10 15:50:23 +0000

    Minor changes to printing

--- serv.js
+++ serv.js
@@ -51,6 +51,6 @@ server.on("close", function(){
 });
 
 server.listen(PORT, ADDRESS, function() {
-        console.log("Server listening to Port %s %s", PORT, server.address().address);
+        console.log("Server listening to Port %s. Server Addess: %s", PORT, server.address().address);
 });
 
